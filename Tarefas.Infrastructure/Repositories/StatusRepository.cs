9dea6a3bd98dae63064819ca24db8b3b65cfe7f3
Augusto Vieira <cv.augusto2@gmail.com>
2023-02-28 17:02:45 -0300

    [WIP] Implementando Repositories

--- Tarefas.Infrastructure/Repositories/StatusRepository.cs
+++ Tarefas.Infrastructure/Repositories/StatusRepository.cs
@@ -5,6 +5,7 @@ namespace Tarefas.Infrastructure.Repositories;
 
 public class StatusRepository : IStatusRepository
 {
+    //TODO: Continue a partir daqui -> https://github.com/CodeMazeBlog/onion-architecture-aspnetcore/blob/7e7f37bb2a8722cddb9d584c7c561556097f5b5f/OnionArchitecutre/Persistence/Repositories/AccountRepository.cs#L18
     private readonly RepositoryDbContext _context;
     public StatusRepository(RepositoryDbContext context) => _context = context;
 }
